b59401bae3881f2b6e0c634e352fa57e4518961b
dylanfetch <dylanfetch@gmail.com>
2025-06-30 21:24:15 -0400

    Improved Solution to Contains Duplicate Problem

--- src/contains_duplicate.py
+++ src/contains_duplicate.py
@@ -2,13 +2,10 @@ from typing import List
 
 class Solution:
     def hasDuplicate(self, nums: List[int]) -> bool:
-        hash_table = {}
+        hash_table = set()
         for num in nums:
-            if num not in hash_table:
-                hash_table[num] = 1
-            else:
-                return True
-            
+            if num in hash_table: return True
+            hash_table.add(num)
         return False
     
 if __name__ == "__main__":
