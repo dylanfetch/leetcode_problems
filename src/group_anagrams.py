87fe0a76eaba4301696f9a881c626a4e01282673
dylanfetch <dylanfetch@gmail.com>
2025-07-05 19:57:36 -0400

    Improved Group Anagrams Solution

--- src/group_anagrams.py
+++ src/group_anagrams.py
@@ -3,15 +3,11 @@ from collections import defaultdict
 
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        list_of_dicts = []
+        hash_table = defaultdict(list)
         for string in strs:
-            hash_table = defaultdict(int)
+            char_count = [0] * 26
             for char in string:
-                hash_table[char] += 1
-            list_of_dicts.append((hash_table,string))
+                char_count[ord(char) - ord('a')] += 1
+            hash_table[tuple(char_count)].append(string)
 
-        dict_of_dicts = defaultdict(list)
-        for hash_table,string in list_of_dicts:
-            dict_of_dicts[frozenset(hash_table.items())].append(string)
-
-        return list(dict_of_dicts.values())
+        return list(hash_table.values())
