5674a916f67757e320998f60314be6bfd6928603
dylanfetch <dylanfetch@gmail.com>
2025-07-13 16:15:06 -0400

    Product of Array Except Self - Brute Force

---
+++ tests/test_product_of_array_except_self.py
@@ -0,0 +1,13 @@
+import pytest
+from src.product_of_array_except_self import Solution
+
+@pytest.mark.parametrize(
+    "nums, expected",
+    [
+        ([1, 2, 3, 4], [24, 12, 8, 6]),
+        ([-1, 1, 0, -3, 3], [0, 0, 9, 0, 0])
+    ]
+)
+
+def test_product_of_array_except_self(nums, expected):
+    assert Solution().productExceptSelf(nums = nums) == expected
